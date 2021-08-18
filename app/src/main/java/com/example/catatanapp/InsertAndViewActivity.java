1ca5480498930a269f6e68ba50cbbe89787cce9a
aisyahharum <aisyahharum003@gmail.com>
2021-08-18 21:08:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/catatanapp/InsertAndViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.catatanapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertAndViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_and_view);
+    }
+}
